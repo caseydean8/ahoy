8fa466167ddb1afbd112174c6aba8dd2a9db6521
Casey Carroll <caseydean8@gmail.com>
2019-10-02 08:09:03 -0400

    app displays with jumbotron

---
+++ ahoy/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ShipDiv from "./components/Ship";
+import Jumbotron from './components/Jumbotron';
+
+function App() {
+  return (
+    <div className="container">
+      <Jumbotron />
+      <ShipDiv />
+    </div>
+  )
+}
+
+export default App;
---
+++ ahoy/src/components/Ship.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ShipDiv() {
+    return (
+        <div>
+            <img src="" alt=""></img>
+        </div>
+    )
+}
+
+export default ShipDiv;
