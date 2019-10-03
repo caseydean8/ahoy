0e90329cc6fbf4213d16d4f8021995c5b185b764
Casey Carroll <caseydean8@gmail.com>
2019-10-03 07:28:12 -0400

    ShipCard now renders from map function

--- ahoy/src/App.js
+++ ahoy/src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Jumbotron from './components/Jumbotron';
 import Wrapper from './components/Wrapper';
 import ShipCard from './components/ShipCard'
 import ships from './ships.json';
 
-function App() {
-  return (
-    <div className="container">
-      <Jumbotron />
-      <Wrapper />
-      <ShipCard 
-      image={ships[0].image}
-      />
-      <ShipCard 
-      image={ships[1].image}
-      />
-    </div>
-  )
+class App extends Component {
+  state = {
+    ships
+  };
+
+
+  render() {
+    return (
+      <div className="container">
+        <Jumbotron />
+        <Wrapper>
+          {this.state.ships.map(ship => (
+            <ShipCard 
+              image={ship.image}
+            />
+          ))}
+        </Wrapper>
+      </div>
+    );
+  }
 }
 
 export default App;
+
