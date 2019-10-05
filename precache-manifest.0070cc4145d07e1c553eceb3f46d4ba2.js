1b64c9a4e7cf6c80cc8e80b3faa79a68d0022963
Casey Carroll <caseydean8@gmail.com>
2019-10-04 21:03:25 -0400

    Updates

---
+++ precache-manifest.0070cc4145d07e1c553eceb3f46d4ba2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86b9e8834363234a659026bb2d4556f",
+    "url": "/ahoy/index.html"
+  },
+  {
+    "revision": "33910cff9ac3e7497386",
+    "url": "/ahoy/static/css/main.2f22dab1.chunk.css"
+  },
+  {
+    "revision": "bd7f509f62be889d1e54",
+    "url": "/ahoy/static/js/2.6671482d.chunk.js"
+  },
+  {
+    "revision": "33910cff9ac3e7497386",
+    "url": "/ahoy/static/js/main.3ccc07c1.chunk.js"
+  },
+  {
+    "revision": "8dece22424fdcc3645ef",
+    "url": "/ahoy/static/js/runtime-main.a66f5d3d.js"
+  }
+]);
